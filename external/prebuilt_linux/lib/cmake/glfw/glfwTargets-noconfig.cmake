d1b6ecb5e88c14be359b19815add41d63bbc83ad
Sean James <seanjames777@gmail.com>
2015-01-24 22:47:46 -0500

    Update linux glfw

--- external/prebuilt_linux/lib/cmake/glfw/glfwTargets-noconfig.cmake
+++ external/prebuilt_linux/lib/cmake/glfw/glfwTargets-noconfig.cmake
@@ -3,45 +3,17 @@
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
-SET(CMAKE_IMPORT_FILE_VERSION 1)
-
-# Compute the installation prefix relative to this file.
-GET_FILENAME_COMPONENT(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
-GET_FILENAME_COMPONENT(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
-GET_FILENAME_COMPONENT(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
-GET_FILENAME_COMPONENT(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "glfw" for configuration ""
-SET_PROPERTY(TARGET glfw APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
-SET_TARGET_PROPERTIES(glfw PROPERTIES
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libglfw.so.3.1"
-  IMPORTED_SONAME_NOCONFIG "libglfw.so.3"
+set_property(TARGET glfw APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(glfw PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "C"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libglfw3.a"
   )
 
-LIST(APPEND _IMPORT_CHECK_TARGETS glfw )
-LIST(APPEND _IMPORT_CHECK_FILES_FOR_glfw "${_IMPORT_PREFIX}/lib/libglfw.so.3.1" )
-
-# Loop over all imported files and verify that they actually exist
-FOREACH(target ${_IMPORT_CHECK_TARGETS} )
-  FOREACH(file ${_IMPORT_CHECK_FILES_FOR_${target}} )
-    IF(NOT EXISTS "${file}" )
-      MESSAGE(FATAL_ERROR "The imported target \"${target}\" references the file
-   \"${file}\"
-but this file does not exist.  Possible reasons include:
-* The file was deleted, renamed, or moved to another location.
-* An install or uninstall procedure did not complete successfully.
-* The installation package was faulty and contained
-   \"${CMAKE_CURRENT_LIST_FILE}\"
-but not all the files it references.
-")
-    ENDIF()
-  ENDFOREACH()
-  UNSET(_IMPORT_CHECK_FILES_FOR_${target})
-ENDFOREACH()
-UNSET(_IMPORT_CHECK_TARGETS)
-
-# Cleanup temporary variables.
-SET(_IMPORT_PREFIX)
+list(APPEND _IMPORT_CHECK_TARGETS glfw )
+list(APPEND _IMPORT_CHECK_FILES_FOR_glfw "${_IMPORT_PREFIX}/lib/libglfw3.a" )
 
 # Commands beyond this point should not need to know the version.
-SET(CMAKE_IMPORT_FILE_VERSION)
+set(CMAKE_IMPORT_FILE_VERSION)
