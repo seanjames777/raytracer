48f233306399d231c1e9cd99b62ba078f64f1fd0
Sean James <seanjames777@gmail.com>
2015-05-13 16:32:16 -0400

    Don't allocate depth buffer

--- src/util/glimagedisplay.cpp
+++ src/util/glimagedisplay.cpp
@@ -66,6 +66,8 @@ void GLImageDisplay::worker_thread() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DEPTH_BITS, 0);
+    glfwWindowHint(GLFW_STENCIL_BITS, 0);
 
     if (!(window = glfwCreateWindow(width, height, "Window", NULL, NULL))) {
         printf("Error opening glfw window\n");
