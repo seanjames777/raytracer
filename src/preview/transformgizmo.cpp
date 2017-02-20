0cce60d91f7c717aaedef6b07b0b86ae5226c87f
Sean James <sean_james@apple.com>
2017-02-19 23:24:31 -0800

    Add comments to transform gizmo

--- src/preview/transformgizmo.cpp
+++ src/preview/transformgizmo.cpp
@@ -12,6 +12,7 @@ void transformGizmo(
     std::vector<SolidVertex> & vertices,
     std::vector<SolidDrawCmd> & commands)
 {
+    // Decompose incoming transformation matrix into position, rotation, and scale
     float4x4 tp = transpose(transform);
 
     float3 c0 = tp.rows[0].xyz();
@@ -36,7 +37,7 @@ void transformGizmo(
 
     float4x4 invViewProjection = inverse(viewProjection);
 
-    // Compute gizmo scale
+    // Compute uniform gizmo size in screen space
     {
         // Transform gizmo position into screen space
         float4 positionNDC(origin, 1);
@@ -61,7 +62,7 @@ void transformGizmo(
 
     Ray ray;
 
-    // Compute view ray
+    // Compute view ray in world space
     {
         float2 ndc = (mousePos - viewPos) / viewSize;
         ndc = ndc * 2.0f - 1.0f;
@@ -84,17 +85,20 @@ void transformGizmo(
         float3(0, 0, 1)
     };
 
-    // Don't allow world space scaling because it leads to skewing
+    // If we are using the object's coordinate system, transform the local axes into world space.
+    // Don't allow world space scaling because it leads to skewing.
     if (state.local || state.mode == TransformModeScale)
         for (int i = 0; i < 3; i++)
             axes[i] = rotation * axes[i];
 
+    // Construct planes from axes
     Plane planes[3] = {
         Plane(axes[0], dot(origin, axes[0])),
         Plane(axes[1], dot(origin, axes[1])),
         Plane(axes[2], dot(origin, axes[2]))
     };
 
+    // Initialize colors to red, green, blue for X, Y, and Z
     float3 axisColors[3];
     float3 quadColors[3];
 
@@ -103,62 +107,82 @@ void transformGizmo(
         quadColors[i] = float3(i == 0 ? 1.0f : 0.0f, i == 1 ? 1.0f : 0.0f, i == 2 ? 1.0f : 0.0f);
     }
 
+    // If we are in the middle of a transformation, compute delta transformation.
     if (state.active) {
+        // If the user released the mouse, stop
         if (!mouseDown) {
             state.active = false;
         }
         else {
             float hitDist;
 
+            // Compute the intersection with the initially selected plane. Then modify the position, rotation,
+            // and scale of the transformation.
             if (planes[state.selectedPlane].intersects(ray, hitDist)) {
                 float3 hitPos = ray.at(hitDist) - origin;
 
-                float3 delta = hitPos - state.selectionOffset;
-                float3 constrainedDelta = 0;
-
-                // TODO: can just filter out the unselected axes directly
-                for (int i = 0; i < 3; i++) {
-                    if ((1 << i) & state.selectedAxes)
-                        constrainedDelta = constrainedDelta + dot(axes[i], delta) * axes[i];
-                }
-
                 if (state.mode == TransformModeTranslation) {
+                    // Compute difference between previous frame's collision point and the current point
+                    float3 delta = hitPos - state.selectionOffset;
+                    float3 constrainedDelta = 0;
+
+                    // Constrain delta to the active axis/axes
+                    for (int i = 0; i < 3; i++)
+                        if ((1 << i) & state.selectedAxes)
+                            constrainedDelta = constrainedDelta + dot(axes[i], delta) * axes[i];
+
+                    // Offset origin by constrained delta
                     origin = constrainedDelta + origin;
-                    // Does not affect offset
+
+                    // Translation does not affect the selection offset
                 }
                 else if (state.mode == TransformModeScale) {
                     float3 scaleAmount(1, 1, 1);
 
-                    for (int i = 0; i < 3; i++) {
+                    // Find scale factor along all active axes
+                    for (int i = 0; i < 3; i++)
                         if ((1 << i) & state.selectedAxes)
                             scaleAmount[i] = max(dot(axes[i], hitPos), 0.00001f) / max(dot(axes[i], state.selectionOffset), 0.00001f);
-                    }
+
+                    // Adjust scale. Scale is always done in the local coordinate system.
                     scale = scaleAmount * scale;
 
-                    // Scales offset
+                    // The selection offset should be scaled as well because the local coordinate system has
+                    // been scaled. However, the selection offset is in world space, so we need to project
+                    // the scale along the world space basis vectors.
                     state.selectionOffset = scaleAmount[0] * dot(state.selectionOffset, axes[0]) * axes[0] +
-                                     scaleAmount[1] * dot(state.selectionOffset, axes[1]) * axes[1] +
-                                     scaleAmount[2] * dot(state.selectionOffset, axes[2]) * axes[2];
+                                            scaleAmount[1] * dot(state.selectionOffset, axes[1]) * axes[1] +
+                                            scaleAmount[2] * dot(state.selectionOffset, axes[2]) * axes[2];
                 }
                 else if (state.mode == TransformModeRotation) {
+                    // Normalize the selection offset
                     float3 S_norm = normalize(state.selectionOffset);
 
+                    // Project the collision point (relative to the origin) onto the selection offset
                     float3 HonS = dot(hitPos, S_norm) * S_norm;
+
+                    // Subtract the projection from the collision point. This gives us two perpendicular
+                    // vectors.
                     float3 HoffS = hitPos - HonS;
 
+                    // Find the angle between the selection offset and the collision point. This is how
+                    // much we need to rotate the selection offset to place it under the cursor.
                     float theta = atan2(length(HoffS), length(HonS));
 
+                    // We may need to swap clockwise/counterclockwise rotation as the angle computed
+                    // previously is not signed.
                     if (dot(cross(S_norm, hitPos), planes[state.selectedPlane].normal) < 0.0f)
                         theta = -theta;
 
+                    // Compute a rotation matrix around the normal of the place we are working in
                     float3x3 rotationMatrix = upper3x3(::rotation(planes[state.selectedPlane].normal, theta));
 
+                    // Adjust the transform's rotation matrix and rotate the selection offset
                     rotation = rotationMatrix * rotation;
-
-                    // Rotates offset
                     state.selectionOffset = rotationMatrix * state.selectionOffset;
                 }
 
+                // Reconstruct the transformation matrix from the adjusted position, rotation, and scale
                 float4x4 fullRotation;
                 fullRotation.rows[0] = float4(rotation.rows[0], 0);
                 fullRotation.rows[1] = float4(rotation.rows[1], 0);
@@ -167,6 +191,7 @@ void transformGizmo(
                 transform = translation(origin) * fullRotation * ::scale(scale);
             }
 
+            // Highlight the active axes
             for (int i = 0; i < 3; i++) {
                 if ((1 << i) & state.selectedAxes)
                     axisColors[i] = float3(1, 1, 0);
@@ -176,12 +201,14 @@ void transformGizmo(
             }
         }
     }
+    // Otherwise, we have not started a transformation. Look for intersections with gizmo geometry.
     else {
         int hoveredAxes = 0;
         int hoveredPlane;
         float hoveredDist = INFINITY;
 
         if (state.mode == TransformModeTranslation || state.mode == TransformModeScale) {
+            // Start by picking the axis with the closest collision point
             for (int pickAxis = 0; pickAxis < 3; pickAxis++) {
                 // TODO: pick the plane with the greatest precision?
                 for (int pickPlane = 0; pickPlane < 3; pickPlane++) {
@@ -206,6 +233,7 @@ void transformGizmo(
                 }
             }
 
+            // If there is a 2 axis quad which is closer, use it instead
             for (int pickPlane = 0; pickPlane < 3; pickPlane++) {
                 int axis0 = (pickPlane + 1) % 3;
                 int axis1 = (pickPlane + 2) % 3;
@@ -228,6 +256,7 @@ void transformGizmo(
             }
         }
         else if (state.mode == TransformModeRotation) {
+            // Pick the ring with the closest collision point
             for (int pickPlane = 0; pickPlane < 3; pickPlane++) {
                 float hitDist;
                 if (planes[pickPlane].intersects(ray, hitDist)) {
@@ -247,6 +276,7 @@ void transformGizmo(
         if (hoveredAxes != 0) {
             state.hovered = true;
 
+            // If we have a collision and the mouse is down, start the transformation
             if (mouseDown) {
                 state.selectedAxes = hoveredAxes;
                 state.selectionOffset = ray.at(hoveredDist) - origin;
@@ -254,6 +284,7 @@ void transformGizmo(
                 state.active = true;
             }
 
+            // Highlight the hovered axis
             for (int i = 0; i < 3; i++) {
                 if ((1 << i) & hoveredAxes)
                     axisColors[i] = float3(1, 1, 0);
@@ -266,6 +297,7 @@ void transformGizmo(
             state.hovered = false;
     }
 
+    // Compute transformation matrix for gizmo geometry
     float4x4 gizmoTransform = translation(origin);
 
     if (state.local || state.mode == TransformModeScale) {
@@ -277,6 +309,7 @@ void transformGizmo(
         gizmoTransform = gizmoTransform * fullRotation;
     }
 
+    // Draw gizmo geometry
     if (state.mode == TransformModeTranslation || state.mode == TransformModeScale) {
         drawAxes(gizmoTransform, 8, axisRadius, 0.66f * axisLength, 0.11f * axisLength, 0.33f * axisLength, float4(axisColors[0], 1), float4(axisColors[1], 1), float4(axisColors[2], 1), vertices, commands);
 
